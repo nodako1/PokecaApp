a911901fccd4294ac9d3eb701cbd26ea13d31eb0
KOICHI NODA <79571927+nodako1@users.noreply.github.com>
2025-10-07 17:55:22 +0900

    Add files via upload

---
+++ PokecaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokecaApp2App.swift
+//  PokecaApp2
+//
+//  Created by Koichi Noda on 2025/10/05.
+//
+
+import SwiftUI
+
+@main
+struct PokecaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CityLeagueListView()
+        }
+    }
+}
